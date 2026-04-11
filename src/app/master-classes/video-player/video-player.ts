d47eda6d87e947685ec48e3fb787e62340f13674
krychuq <kn@payment-holding.com>
2026-04-11 22:11:49 +0200

    EXPIRED fixed

--- src/app/master-classes/video-player/video-player.ts
+++ src/app/master-classes/video-player/video-player.ts
@@ -5,6 +5,7 @@ import { McFooter } from '../footer/footer';
 import { McHeader } from '../mc-header/mc-header';
 import { VideoService } from '../../services/video.service';
 import {McPrivacyNotice} from '../privacy-notice/privacy-notice';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-video-player',
@@ -24,11 +25,21 @@ export class VideoPlayer implements OnInit {
   constructor(
     private sanitizer: DomSanitizer,
     private videoService: VideoService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-      this.videoService.videoUrl()
-    );
+    const videoUrl = this.videoService.videoUrl();
+    this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
+
+    try {
+      const expires = new URL(videoUrl).searchParams.get('expires');
+      console.log(expires, Number(expires) * 1000 < Date.now());
+      if (expires && Number(expires) * 1000 < Date.now()) {
+        this.router.navigate(['video/expired']);
+        return;
+      }
+    } catch {}
+
   }
 }
